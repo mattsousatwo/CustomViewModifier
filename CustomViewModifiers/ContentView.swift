11ed5ac4ea1dca51115897238601427bc60b976c
Matthew Sousa <mattsousatwo@gmail.com>
2021-10-29 15:11:09 -0400

    Initial Commit

---
+++ CustomViewModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomViewModifiers
+//
+//  Created by Matthew Sousa on 10/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomViewModifiers/CustomViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewModifiersApp.swift
+//  CustomViewModifiers
+//
+//  Created by Matthew Sousa on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
